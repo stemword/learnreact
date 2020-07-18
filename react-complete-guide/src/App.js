8e0dd6b1688be9a1c294ea130a6302a40b744ec5
kshitij soni <kshitij206@gmail.com>
2020-07-18 18:50:57 +0530

    new componenet person

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hi I am react app </h1>
+        <Person />
       </div>
     );
   }
