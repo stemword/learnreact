b5d7d9f38312101a7dd333b11417c686fee8e4cf
kshitij soni <kshitij.soni@petpooja.com>
2020-07-19 21:11:32 +0530

    state example

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -3,13 +3,44 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      {
+        name: 'Max' , age : 28
+      },
+      {
+        name: 'Manu' , age : 29
+      },
+      {
+        name: 'Maxsta' , age : 26
+      }
+    ]
+  }
+
+  SwitchNameHandler = () => {
+    //Dont do this this.state.persons[0].name = "kshitij";
+    this.setState({
+      persons: [
+        {
+          name: 'Max1' , age : 28
+        },
+        {
+          name: 'Manu1' , age : 29
+        },
+        {
+          name: 'Maxsta1' , age : 26
+        }
+      ]
+    });
+  }
   render() {
     return (
       <div className="App">
         <h1>Hi I am react app </h1>
-        <Person name="hey" age="25"/>
-        <Person name="hey2" age="29">My hobbies are photgraphy</Person>
-        <Person name="hey3" age="27"/>
+        <button onClick={this.SwitchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].name}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies are photgraphy</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
   }
