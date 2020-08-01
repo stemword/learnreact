af83716f40af0a5c858b5f88d7e2fe8329ed1b39
kshitij soni <kshitij206@gmail.com>
2020-08-01 19:45:36 +0530

    merge code

--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,23 +1,17 @@
 import React from 'react';
-import './Person.css';
-import styled from 'styled-components';
-//import Radium from 'radium';
 
-const StyleDiv = styled.div`
-  width: 60%;
-  margin: 16px auto;
-  border: 1px solid #eeeeee;
-  padding: 16px;
-  text-align: center;
+import classes from './Person.css';
 
-
-
-@media (min-width: 500px) {
-		width: 450px;
-}`;	
-const person = (props) => {
-	return <StyleDiv>
-	<p onClick={props.click}>I am a {props.name} and I am {props.age} years old.</p></StyleDiv>
-}
+const person = props => {
+  return (
+    <div className={classes.Person}>
+      <p onClick={props.click}>
+        I'm {props.name} and I am {props.age} years old!
+      </p>
+      <p>{props.children}</p>
+      <input type="text" onChange={props.changed} value={props.name} />
+    </div>
+  );
+};
 
 export default person;
