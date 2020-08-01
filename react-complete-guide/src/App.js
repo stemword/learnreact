af79447d73408616c98947c4a5fadd0590032bed
kshitij soni <kshitij.soni@petpooja.com>
2020-08-01 09:08:14 +0530

    assignment module

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -6,41 +6,71 @@ class App extends Component {
   state = {
     persons: [
       {
-        name: 'Max' , age : 28
+        id:1,name: 'Max' , age : 28
       },
       {
-        name: 'Manu' , age : 29
+        id:2,name: 'Manu' , age : 29
       },
       {
-        name: 'Maxsta' , age : 26
+        id:3,name: 'Maxsta' , age : 26
       }
-    ]
+    ],
+    showPersons:false
   }
 
-  SwitchNameHandler = () => {
-    //Dont do this this.state.persons[0].name = "kshitij";
+  deletePersonHandler = (personIndex) => {
+    
+    //const persons = this.state.persons.slice();
+    const persons = [...this.state.persons];
+    persons.splice(personIndex , 1); 
+    this.setState({persons:persons})
+  }
+
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({showPersons : !doesShow});
+  }
+
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p=>{
+      return p.id == id;
+    });
+
+    const person = {...this.state.persons[personIndex]};
+    person.name = event.target.value;
+    const persons = [...this.state.persons];
+    persons[personIndex] =  person;
     this.setState({
-      persons: [
-        {
-          name: 'Max1' , age : 28
-        },
-        {
-          name: 'Manu1' , age : 29
-        },
-        {
-          name: 'Maxsta1' , age : 26
-        }
-      ]
+      persons:persons
     });
   }
   render() {
+    var persons = null;
+    if(this.state.showPersons) {
+      persons = (<div>
+        {this.state.persons.map((person , index) => {
+          return <Person 
+          click = {() => this.deletePersonHandler(index)}
+          name={person.name} 
+          age={person.name}
+          key={person.id}
+          changed={(event) => this.nameChangedHandler(event,person.id)}
+          />
+        })}
+        </div>)
+    }
+    const style = {
+      backgroundColor : 'white',
+      font:"inherit",
+      border:'1px solid blue',
+      padding:'8px',
+      cursor:'pointer'
+    };
     return (
       <div className="App">
         <h1>Hi I am react app </h1>
-        <button onClick={this.SwitchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].name}/>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies are photgraphy</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+        <button style={style} onClick={() => this.togglePersonsHandler()}>Switch Name</button>
+        {persons}
       </div>
     );
   }
--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import './Person.css';
 const person = (props) => {
-	return <p>I am a {props.name} and I am {props.age} years old.</p>
+	return <p className="Person" onClick={props.click}>I am a {props.name} and I am {props.age} years old.</p>
 }
 
 export default person;
